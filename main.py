b9e509aafbbc905d643d6ccc92e76b53f0de32db
esnwhy <esnwhy.acct@gmail.com>
2025-06-14 20:30:27 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import time
 
 app = Flask(__name__)
 
-# グローバルな記録用ディクショナリ
 last_signals = {}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    print("Received TEST webhook:", data)
+    print("Received TEST webhook:", data, flush=True)
 
     signal = data.get("signal")
     symbol = data.get("symbol")
     now = time.time()
 
     key = f"{symbol}_{signal}"
-    print(f"Current last_signals: {last_signals}")
+    print(f"Current last_signals: {last_signals}", flush=True)
 
     if key in last_signals and now - last_signals[key] < 60:
-        print(f"[SKIP] Duplicate {signal} for {symbol} (within 60s)")
+        print(f"[SKIP] Duplicate {signal} for {symbol} (within 60s)", flush=True)
         return "Duplicate ignored", 200
 
     last_signals[key] = now
-    print(f"[TEST] Accepted signal: {signal} for {symbol} at {now}")
+    print(f"[TEST] Accepted signal: {signal} for {symbol} at {now}", flush=True)
     return "Test order received", 200
 
 if __name__ == '__main__':
