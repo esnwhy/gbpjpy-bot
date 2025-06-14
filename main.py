5f70a806d069c4d62af6f7194bfad07245435588
esnwhy <esnwhy.acct@gmail.com>
2025-06-14 18:57:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ def webhook():
         return "Test order received", 200
 
     return "Invalid signal", 400
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
